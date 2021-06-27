717a4eae7e7b087b9c36e0a3bbfdddfcc4eae333
Devesh Kumar Sharma <deveshkumarsharma475@gmail.com>
2021-06-28 03:17:07 +0530

    second commit

--- musicplayer/settings.py
+++ musicplayer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd4ii#o7hbz+!nu)&rk+@tj@bq4244)9qclu=_dl%mvr7jfv3@!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
